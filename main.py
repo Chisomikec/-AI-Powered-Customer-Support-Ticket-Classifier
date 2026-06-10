eb7a5a0eaeba4f17a4d90b39990bba452a2fabe5
Chisom Ikechukwu (k23086553) <chisom.ikechukwu@kcl.ac.uk>
2026-06-10 16:19:50 +0100

    add comments

--- main.py
+++ main.py
@@ -7,29 +7,27 @@ import json
 
 load_dotenv()
 
+class user_query(BaseModel):   # Pydantic model defines the expected format of the incoming request
+    messages: list[str]         # message format: list of strings
 
-class user_query(BaseModel):
-    messages: list[str]
-
-app = FastAPI()
+app = FastAPI()     # fastapi instance
 
 genai.configure(api_key=os.environ['GEMINI_API_KEY'])
 
 
-@app.post("/classify")
+@app.post("/classify")     # decorator that registers the function below as a POST endpoint at /classify
 def user_query_classifier(requests: user_query):
-    model=genai.GenerativeModel(model_name="gemini-3.5-flash")
+    model=genai.GenerativeModel(model_name="gemini-3.5-flash")      # google gemini ai model
 
-    results= []
-    for message in requests.messages:
-        if not message:
-            return {"error": "Message cannot be empty"}
+    results= []     # store answers from model
+    for message in requests.messages:   # loop through user's query
+        if not message:     # handle empty query
+            return {"error": "Message cannot be empty"} 
         
         try: 
             
-            print(message)
-            # client = genai.Client()
-        
+            # print(message)
+            
             response = model.generate_content( contents= f'''You are a virtual customer service for a fintech company. 
                         Your role is to analyse the user's full message and classify it into one of the following catigories:
                         - Billing
@@ -41,21 +39,21 @@ def user_query_classifier(requests: user_query):
                         Return your response ONLY as a JSON object with two keys: category and confidence. Nothing else.'''
         )
         
-            print(response.text)
+            # print(response.text)
             
-            cleaned = response.text.strip().removeprefix("```json").removesuffix("```").strip()
-            result = json.loads(cleaned)
-            category = result['category']
+            cleaned = response.text.strip().removeprefix("```json").removesuffix("```").strip()  # clean version of gemini response in string format
+            result = json.loads(cleaned)    # covert response to python object
+            category = result['category']   
             confidence = result['confidence']
 
-            results.append({"category": category, "confidence": confidence})
+            results.append({"category": category, "confidence": confidence})    # append response for a message in the correct format
 
             
-        except Exception as e:
+        except Exception as e:   # catch any unexpected errors eg Gemini returning unparseable response
             print(e)
             return {"error": "Cannot classify your request, please speak to a human customer service"}
     return results
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn  
+    uvicorn.run(app, host="0.0.0.0", port=8000)     # run fastapi server using host and port
